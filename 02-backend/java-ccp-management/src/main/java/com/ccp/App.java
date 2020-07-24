62e477e92b705a7824362fac855a85299c9ed471
damte <d.gaite.cruz@gmail.com>
2020-07-24 14:30:57 +0300

    working config of Hibernate updated

--- 02-backend/java-ccp-management/src/main/java/com/ccp/App.java
+++ 02-backend/java-ccp-management/src/main/java/com/ccp/App.java
@@ -3,25 +3,38 @@ package com.ccp;
 import com.ccp.config.HibernateConfig;
 import com.ccp.entity.Employee;
 import com.ccp.entity.Project;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 public class App {
     public static void main(String[] args) {
-        final EntityManagerFactory entityManagerFactory = HibernateConfig.buildEntityManagerFactory();
-
-        final EntityManager entityManager = entityManagerFactory.createEntityManager();
-//        Project project = Project.builder().name("Estonia National Opera House").duration(18).income(50000).build();
-//        Employee employee = Employee.builder()
-//                .name("Ramón")
-//                .surname("Ramirez").project(project)
-//                .build();
-
-        entityManager.getTransaction().begin();
-//        entityManager.persist(employee);
+//        final EntityManagerFactory entityManagerFactory = HibernateConfig.buildEntityManagerFactory();
+//        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+/*        entityManager.getTransaction().begin();
+        entityManager.persist(employee);
         entityManager.getTransaction().commit();
         entityManager.close();
         entityManagerFactory.close();
+ */
+        final SessionFactory sessionFactory = HibernateConfig.buildSessionFactory();
+        final Session session = sessionFactory.openSession();
+
+        Project project = Project.builder().name("Estonia National Opera House").duration(18).income(50000).build();
+        Employee employee = Employee.builder()
+                .name("Ramón")
+                .surname("Ramirez").project(project)
+                .build();
+
+        session.beginTransaction();
+        session.save(project);
+        session.save(employee);
+        System.out.println(employee.toString() + " inserted successfully");
+        session.getTransaction().commit();
+        session.close();
+        sessionFactory.close();
+
     }
 }
--- 02-backend/java-ccp-management/src/main/java/com/ccp/config/HibernateConfig.java
+++ 02-backend/java-ccp-management/src/main/java/com/ccp/config/HibernateConfig.java
@@ -1,12 +1,34 @@
 package com.ccp.config;
 
+import com.ccp.entity.Employee;
+import com.ccp.entity.Project;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class HibernateConfig {
-    public static EntityManagerFactory buildEntityManagerFactory() {
-        EntityManagerFactory entityManagerFactory =
-                Persistence.createEntityManagerFactory("org.hibernate.ccp-management");
-        return entityManagerFactory;
+//    public static EntityManagerFactory buildEntityManagerFactory() {
+//        EntityManagerFactory entityManagerFactory =
+//                Persistence.createEntityManagerFactory("org.hibernate.ccp-management");
+//        return entityManagerFactory;
+//    }
+
+    public static SessionFactory buildSessionFactory(){
+        try{
+            final Configuration configuration = new Configuration();
+            configuration.configure("hibernate.cfg.xml");
+            final ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+            return configuration.configure().buildSessionFactory();
+
+        }catch (Exception ex){
+            System.err.println("Initial SessionFactory Creation Failed, " + ex.getMessage());
+            throw  ex;
+        }
     }
 }
